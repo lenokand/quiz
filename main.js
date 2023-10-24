d9da7cb70395c282b98eae3587e09e0c541a46a1
Marwa_Ibrahim <marwa.ibrahim@powercoders.org>
2023-10-24 14:49:27 +0200

    simplfy the JS code

--- main.js
+++ main.js
@@ -34,15 +34,105 @@ function shuffleArray(array) {
   }
 
  shuffleArray(questions)
-console.log(questions)
-const questionText = document.getElementById("question-text");
-const optionsList = document.getElementById("options-list");
-const feedbackText = document.getElementById("feedback-text");
-const nextButton = document.getElementById("next-button");
-const resultContainer = document.getElementById("result-container");
-const scoreText = document.getElementById("score-text");
-const messageText = document.getElementById("message-text");
-const timerElement = document.getElementById("timer");
+
+document.addEventListener("DOMContentLoaded", function () {
+    const questions = [
+        {
+            question: "How do you create a new directory in CLI?",
+            options: ["mkdir", "rmdir", "touch", "code"],
+            correctAnswer: "mkdir"
+        },
+        {
+            question: "Which is an inline-level element in HTML?",
+            options: ["article", "fieldset", "aside", "a"],
+            correctAnswer: "a"
+        },
+        {
+            question: "Which one is not a CSS measuring unit?",
+            options: ["pt", "em", "px", "cm"],
+            correctAnswer: "cm"
+        },
+        {
+            question: "What is considered a building block of programming?",
+            options: ["Data type", "Variables", "functions", "all of them"],
+            correctAnswer: "all of them"
+        },
+        {
+            question: "Which is an example of loops in JavaScript?",
+            options: ["if else", "switch", "ternary condition", "while"],
+            correctAnswer: "while"
+        }
+    ];
+
+    let currentQuestionIndex = 0;
+    let score = 0;
+
+    function displayQuestion() {
+        const currentQuestion = questions[currentQuestionIndex];
+        document.getElementById("question-text").textContent = currentQuestion.question;
+
+        const optionsList = document.getElementById("options-list");
+        optionsList.innerHTML = "";
+
+        currentQuestion.options.forEach((option, index) => {
+            const li = document.createElement("li");
+            li.textContent = option;
+            li.classList.add("option");
+            li.addEventListener("click", () => checkAnswer(index));
+            optionsList.appendChild(li);
+        });
+
+        document.getElementById("feedback-text").textContent = "Select an option and click 'Next' to continue";
+        document.getElementById("next-button").style.display = "none";
+    }
+
+    function checkAnswer(selectedIndex) {
+        const currentQuestion = questions[currentQuestionIndex];
+        const feedbackText = document.getElementById("feedback-text");
+
+        if (selectedIndex === -1) {
+            feedbackText.textContent = `Time's up! The correct answer is: ${currentQuestion.correctAnswer}`;
+        } else if (currentQuestion.options[selectedIndex] === currentQuestion.correctAnswer) {
+            feedbackText.textContent = "Correct!";
+            score++;
+        } else {
+            feedbackText.textContent = `Wrong. The correct answer is: ${currentQuestion.correctAnswer}`;
+        }
+
+        document.getElementById("next-button").style.display = "block";
+        document.querySelectorAll(".option").forEach(option => option.removeEventListener("click", checkAnswer));
+    }
+
+    document.getElementById("next-button").addEventListener("click", () => {
+        if (currentQuestionIndex < questions.length - 1) {
+            currentQuestionIndex++;
+            displayQuestion();
+        } else {
+            displayResult();
+        }
+    });
+
+    function displayResult() {
+        const resultContainer = document.getElementById("result-container");
+        resultContainer.style.display = "block";
+
+        document.getElementById("score-text").textContent = `${score} / ${questions.length}`;
+
+        const messageText = document.getElementById("message-text");
+        messageText.textContent = score === questions.length ? "You are a genius!" : "You seem to have had a bad day.";
+    }
+
+    displayQuestion();
+});
+
+// const questionText = document.getElementById("question-text");
+// const optionsList = document.getElementById("options-list");
+// const feedbackText = document.getElementById("feedback-text");
+// const nextButton = document.getElementById("next-button");
+// const resultContainer = document.getElementById("result-container");
+// const scoreText = document.getElementById("score-text");
+// const messageText = document.getElementById("message-text");
+// const timerElement = document.getElementById("timer");
 
 
 
@@ -53,7 +143,7 @@ const timerElement = document.getElementById("timer");
 
 
 // let timer;
-// let timeLeft = 60; // 1 minute in seconds
+// let timeLeft = 60;
 
 // function updateTimerDisplay() {
 //     const minutes = Math.floor(timeLeft / 60);
